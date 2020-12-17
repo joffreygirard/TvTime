d3de816ec996a65890dce356d4c8b81aa6bb2df2
MricMric <aymeric.delabarre@gmail.com>
2020-12-17 13:57:18 +0100

    1 er commit Aymeric

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss']/*,
+  template: `
+  <ion-content>
+    <ion-slides pager="true" [options]="slideOpts">
+      <ion-slide>
+        <h1>FILMS À VOIR</h1>
+      </ion-slide>
+      <ion-slide>
+        <h1>PROCHAINEMENT</h1>
+      </ion-slide>
+    </ion-slides>
+  </ion-content>
+`*/
+
 })
 export class Tab2Page {
-
+  // Optional parameters to pass to the swiper instance.
+  // See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
   constructor() {}
 
 }
