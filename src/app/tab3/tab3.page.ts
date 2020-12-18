20a2fa7345631ff4ce8ccc9b0a3074d79d294f35
MricMric <aymeric.delabarre@gmail.com>
2020-12-18 15:28:17 +0100

    Update recherche

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,55 +8,91 @@ import { Component, OnInit } from '@angular/core';
 export class Tab3Page implements OnInit{
 
   
-  isdisplayInfo: boolean;
+  isdisplayInfoFilm: boolean;
+  isdisplayInfoSerie: boolean;
   listeFilms = [];
+  listeSeries = [];
   currentFilmToDisplay: number;
+  currentSerieToDisplay: number;
 
   constructor() {}
 
   ngOnInit(){
     
-    this.isdisplayInfo = false;
+    this.isdisplayInfoFilm = false;
+    this.isdisplayInfoSerie = false;
 
     this.listeFilms[0] = {
-      id: 1,
-      titre: "Inception",
-      date: "21 Juillet 2010",
-      source: "https://www.telerama.fr/sites/tr_master/files/4b88fb8f-a1f8-4432-a222-17c30c201e6d_2.jpg",
-      producteur: "Christopher Nolan",
+      id: 0,
+      titre: "Spider-Man: Homecoming",
+      date: "28 juin 2017",
+      source: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT72uKlaQ_H8LDv6-iAdjjOH_V38w7oCtRAid9d7ievuVmJLDOG",
+      realisateur: "Jon Watts",
+      type: "Action/Aventure",
+      duree: "2h 13m",
+      acteurs: ["Tom Holland","Zendaya","Michael Keaton","Robert Downey Jr."],
+      note: "4,3",
+      synopsis: "Après ses spectaculaires débuts avec les Avengers, le jeune Peter Parker découvre peu à peu sa nouvelle identité, celle de Spider-Man, le superhéros lanceur de toile. Galvanisé par ses expériences précédentes, Peter rentre chez lui auprès de sa tante May, sous l'oeil attentif de son nouveau mentor, …",
     }
     this.listeFilms[1] = {
-      id: 2,
-      titre: "La guerre des étoiles",
-      date: "19 octobre 1977",
-      source: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRjqiljH7-NHwYJo0IEB3l-kxQPBrhUMox3Yx1DRF7Vc6lweYQP",
-      producteur: "George Lucas",
+      id: 1,
+      titre: "Joker",
+      date: "2 octobre 2019",
+      source: "https://fr.web.img6.acsta.net/pictures/19/09/03/12/02/4765874.jpg",
+      realisateur: "Todd Phillips",
+      type: "Crime/Drame",
+      duree: "2h 2m",
+      acteurs: ["Joaquin Phoenix","Robert De Niro","Zazie Beetz","Frances Conroy"],
+      note: "4,5",
+      synopsis: "Dans les années 1980, à Gotham City, Arthur Fleck, un comédien de stand-up raté est agressé alors qu'il ère dans les rues de la ville déguisé en clown. Méprisé de tous et bafoué, il bascule peu à peu dans la folie pour devenir le Joker, un dangereux tueur psychotique.",
     }
-    this.listeFilms[2] = {
-      id: 3,
-      titre: "Ça",
-      date: "5 septembre 2017",
-      source: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7hAv9GXsE4S9qzE60RY-EkQ0U5KLcNRrvRQSYlRs736PqS7WJ",
-      producteur: "Andrés Muschietti",
+
+    this.listeSeries[0] = {
+      id: 0,
+      titre: "Stranger Things",
+      date: "15 juillet 2016",
+      source: "https://fr.web.img3.acsta.net/pictures/19/01/03/10/42/0987125.jpg",
+      realisateur: "Matt Duffer, Ross Duffer",
+      type: "Horreur",
+      duree: "3 saisons",
+      acteurs: ["Millie Bobby Brown","Finn Wolfhard","Noah Schnapp","Gaten Matarazzo","Caleb McLaughlin"],
+      note: "4,9",
+      synopsis: "En 1983, à Hawkins dans l'Indiana, Will Byers disparaît de son domicile. Ses amis se lancent alors dans une recherche semée d'embûches pour le retrouver. Pendant leur quête de réponses, les garçons rencontrent une étrange jeune fille en fuite.",
     }
-    this.listeFilms[3] = {
-      id: 4,
-      titre: "Retour vers le futur",
-      date: "30 octobre 198",
-      source: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSwMYtv4XLYfcCsZiv-Ay5XekYIY5zZP2RMlj68V9g68RAzbwiF",
-      producteur: "Robert Zemeckis",
+    this.listeSeries[1] = {
+      id: 1,
+      titre: "Les 100",
+      date: "19 mars 2014",
+      source: "https://fr.web.img5.acsta.net/pictures/20/05/18/09/30/1101000.jpg",
+      realisateur: "NETFLIX",
+      type: "SF",
+      duree: "7 saisons",
+      acteurs: ["Eliza Taylor","Bob Morley","Marie Avgerop","Lindsey Morgan","Richard Harmon"],
+      note: "4,7",
+      synopsis: "Après une apocalypse nucléaire, les 318 survivants se réfugient dans des stations spatiales et parviennent à y vivre et à se reproduire, atteignant le nombre de 4000 ; 97 ans plus tard, une centaine de jeunes délinquants redescendent sur Terre.",
     }
   }
 
   //Afficher les détail d'un film
-  displayInfo(idFilm) {
-    this.isdisplayInfo = true;
+  displayInfoFilm(idFilm) {
+    this.isdisplayInfoFilm = true;
     this.currentFilmToDisplay = idFilm;
   }
 
   //Fermer les détail du film
-  undisplayInfo() {
-    this.isdisplayInfo = false;
+  undisplayInfoFilm() {
+    this.isdisplayInfoFilm = false;
+  }
+
+  //Afficher les détail d'une série
+  displayInfoSerie(idFilm) {
+    this.isdisplayInfoSerie = true;
+    this.currentSerieToDisplay = idFilm;
+  }
+
+  //Fermer les détail de la série
+  undisplayInfoSerie() {
+    this.isdisplayInfoSerie = false;
   }
 
 }
